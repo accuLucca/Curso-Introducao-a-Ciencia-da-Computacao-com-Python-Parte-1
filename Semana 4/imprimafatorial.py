7d2862670560e73addc5a6cbf9a30bfcea7a0dd4
Lucca Bulara <43175678+accuLucca@users.noreply.github.com>
2021-03-04 12:09:25 -0300

    Semana 4

---
+++ Semana 4/imprimafatorial.py
@@ -0,0 +1,7 @@
+n = int(input("Digite o valor de n: "))
+fatorial = 1
+x = 2
+while n >= x:
+    fatorial = fatorial*x
+    x = x + 1
+print(fatorial)
---
+++ Semana 4/imprimanumeroimpar.py
@@ -0,0 +1,5 @@
+n = int(input("Digite o valor de n: "))
+x = 0
+while x < n:
+    print(2*x+1)
+    x = x + 1
