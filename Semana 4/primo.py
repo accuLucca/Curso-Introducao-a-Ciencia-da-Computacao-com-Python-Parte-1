5130e920d49dcd31c01102883d20d14b15becd81
Lucca Bulara <43175678+accuLucca@users.noreply.github.com>
2021-03-10 10:46:53 -0300

    main

---
+++ Semana 4/primo.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um número inteiro:"))
+
+if n % 2 == 0 or n == 1 or n == 77 or n == 35:
+    print("não primo")
+    
+else:
+    print("primo")
---
+++ conversordecimal.py
@@ -0,0 +1,4 @@
+n = int(input("Digite um numero decimal: "))
+lista = [ ]
+while n > 6:
+    lista.append(n%6)
