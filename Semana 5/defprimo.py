1a50bc3f1a1e4f8d91ac8f29041fa978ce73f4b0
Lucca Bulara <43175678+accuLucca@users.noreply.github.com>
2021-03-13 23:00:16 -0300

    semana 5

---
+++ Semana 5/defprimo.py
@@ -0,0 +1,19 @@
+def maior_primo(x):
+    i = 0
+    while x - i > 1:
+        if isprime(x-i) == 1:
+            return (x-i)
+        else:
+            i += 1
+def isprime(n):
+    if n <= 0:
+        return 0
+    else:
+        i = 1
+        while n - i > 1:
+            if n % (n - i) == 0:
+                return 0
+            i += 1
+            if n - i == 1:
+                return 1
+print(maior_primo(7536))
--- teste.py
+++
@@ -1,3 +0,0 @@
-def oi()
-oi()
-def falacmg("oi")
